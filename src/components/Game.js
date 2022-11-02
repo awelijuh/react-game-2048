569df4a82e7a708348629f50e2086a90fb60b534
stepa <awelijuh@gmail.com>
2022-11-02 18:21:44 +0300

    swipe support

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import Item from "./Item";
-import { Button, Container, Row } from "react-bootstrap";
-import { move, createEmptyField, isFinish, generateNewValue } from "../utils/GameUtils";
+import {Button, Container, Row} from "react-bootstrap";
+import {move, createEmptyField, isFinish, generateNewValue} from "../utils/GameUtils";
+import {useSwipeable} from "react-swipeable";
 
 
 export default function Game(props) {
@@ -9,20 +10,8 @@ export default function Game(props) {
     const [history, setHistory] = useState([])
     const [score, setScore] = useState(0)
 
-    let handleKey = key => {
-        let f = null;
-        if (key.keyCode == '38') {
-            f = move(field, -1, 0)
-        }
-        else if (key.keyCode == '40') {
-            f = move(field, 1, 0)
-        }
-        else if (key.keyCode == '37') {
-            f = move(field, 0, -1)
-        }
-        else if (key.keyCode == '39') {
-            f = move(field, 0, 1)
-        }
+    function handleMove(x, y) {
+        let f = move(field, x, y);
         if (f != null) {
             let nField = generateNewValue(f.field)
             let nScore = score + f.score
@@ -31,10 +20,22 @@ export default function Game(props) {
         }
     }
 
+    let handleKey = key => {
+        if (key.keyCode == '38') {
+            handleMove(-1, 0)
+        } else if (key.keyCode == '40') {
+            handleMove(1, 0)
+        } else if (key.keyCode == '37') {
+            handleMove(0, -1)
+        } else if (key.keyCode == '39') {
+            handleMove(0, 1)
+        }
+    }
+
     useEffect(() => {
         let h = [...history]
-        if (h[h.length - 1]?.field != field) {
-            h.push({ field: field, score: score })
+        if (h[h.length - 1]?.field !== field) {
+            h.push({field: field, score: score})
             setHistory(h)
         }
     }, [field, score])
@@ -54,8 +55,15 @@ export default function Game(props) {
 
     let finished = isFinish(field)
 
+    const swipeHandlers = useSwipeable({
+        onSwipedLeft: e => handleMove(0, -1),
+        onSwipedRight: e => handleMove(0, 1),
+        onSwipedUp: e => handleMove(-1, 0),
+        onSwipedDown: e => handleMove(1, 0),
+    })
+
     return (
-        <div className="" style={{ textAlign: 'center' }}>
+        <div className="" style={{textAlign: 'center'}}>
             <div className="mt-2 d-flex">
                 <div className="ms-auto me-auto">
                     <Button variant="outline-secondary" className="me-1" onClick={() => {
@@ -67,32 +75,39 @@ export default function Game(props) {
                             setScore(h[h.length - 1].score)
                         }
                     }}>Back</Button>
-                    <Button variant="secondary" className="ms-1" onClick={() => { restartGame() }}>Restart</Button>
+                    <Button variant="secondary" className="ms-1" onClick={() => {
+                        restartGame()
+                    }}>Restart</Button>
 
                 </div>
 
             </div>
-            <div className="mt-2" style={{ fontSize: 18 }}>
+            <div className="mt-2" style={{fontSize: 18}}>
                 <b className="">score: </b>
                 <label>{score}</label>
             </div>
-            <div>
-                <div className={"d-flex justify-content-center"} >
-                    <div className={"mt-2"} style={{ position: 'relative' }}>
+            <div {...swipeHandlers}>
+                <div className={"d-flex justify-content-center"}>
+                    <div className={"mt-2"} style={{position: 'relative'}}>
                         <div className={"" + (finished ? " translucent" : "")}>
                             {
                                 field.map((row, i) =>
                                     <Row>
                                         {
-                                            row.map((value, j) => <Item key={i + "_" + j + '_' + value} value={value} />)
+                                            row.map((value, j) => <Item key={i + "_" + j + '_' + value} value={value}/>)
                                         }
                                     </Row>
                                 )
                             }
                         </div>
 
-                        <div className={"m-auto" + (!finished ? " d-none" : '')} style={{ position: "absolute", top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
-                            <label style={{ fontSize: 30, fontWeight: 'bold' }} className="m-auto">Game over</label>
+                        <div className={"m-auto" + (!finished ? " d-none" : '')} style={{
+                            position: "absolute",
+                            top: '50%',
+                            left: '50%',
+                            transform: 'translate(-50%, -50%)'
+                        }}>
+                            <label style={{fontSize: 30, fontWeight: 'bold'}} className="m-auto">Game over</label>
                         </div>
                     </div>
                 </div>
